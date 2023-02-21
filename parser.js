5173390b6d7be4867656184a5cd0925533535bb5
Fadoli <f13@laposte.net>
2023-02-21 22:55:24 +0100

    functionnal parser.js with new functionnality

--- parser.js
+++ parser.js
@@ -29,7 +29,7 @@ function toJS(content) {
         }
         if (line === '}') {
             // Handling of recipes
-            if (current.EvolvedRecipe) {
+            if (current?.EvolvedRecipe) {
 
             }
             current = definitions.pop();
@@ -52,7 +52,16 @@ function toJS(content) {
             return;
         }
         // Recipe are not suported for now
-        if (current.__type__ === 'recipe') {
+        if (current?.__type__ === 'recipe') {
+            if (current.__unparsed__) {
+                current.__unparsed__ = `${current.__unparsed__}\n${line}`;
+            } else {
+                current.__unparsed__ = line;
+            }
+            return;
+        }
+        // Most likely comments, who cares about those
+        if (line.startsWith('/')) {
             return;
         }
         // Handle definitions
@@ -67,5 +76,77 @@ function toJS(content) {
     return output;
 }
 
+function toScript(content, prefix = '') {
+    let output = [];
+    const addToPrefix = '\t';
+    Object.keys(content).forEach((key)=> {
+        // skip internal stuff
+        if (key.startsWith('__')) {
+            return;
+        }
+        const element = content[key];
+
+        if (element.__type__) {
+            output.push(`${prefix}${element.__type__} ${key}`);
+            output.push(`${prefix}{`);
+            if (element.__unparsed__) {
+                element.__unparsed__.split(',').forEach((entry) => {
+                    const line = entry.trim();
+                    if (!line)
+                        return;
+                    output.push(`${addToPrefix}${prefix}${line},`);
+                })
+            }
+            output.push(...toScript(element, `${addToPrefix}${prefix}`));
+            output.push(`${prefix}}`);
+        } else {
+            output.push(`${prefix}${key} = ${element},`);
+        }
+    });
+    if (prefix) {
+        return output;
+    }
+    return output.join('\n');
+}
+
+function comparePatch(base,moded) {
+    let output = {};
+    let hadDif = false;
+    Object.keys(moded).forEach((key) => {
+        const modedValue = moded[key];
+        const baseValue = base[key];
+        if (typeof modedValue === 'object') {
+            let dif = comparePatch(baseValue, modedValue);
+            if (dif) {
+                hadDif = true;
+                output[key] = dif;
+            }
+            return;
+        }
+        if (modedValue !== baseValue) {
+            output[key] = `${baseValue} -> ${modedValue}`;
+            hadDif = true;
+            return;
+        }
+    })
+    if (hadDif) {
+        return output;
+    }
+    return undefined;
+}
+
+/*
 const converted = toJS(fs.readFileSync('./media/scripts/farming.txt', 'utf8'))
 console.log(converted);
+const back = toScript(converted)
+console.log(back);
+fs.writeFileSync('output.txt', back);
+*/
+
+const filesToCheck = ['farming.txt', 'items_food.txt'];
+
+for (const file of filesToCheck) {
+    const orig = toJS(fs.readFileSync('./originals/'+file, 'utf8'))
+    const moded = toJS(fs.readFileSync('./media/scripts/'+file, 'utf8'))
+    fs.writeFileSync(`./changed_${file}`, JSON.stringify(comparePatch(orig,moded),null,4));
+}
